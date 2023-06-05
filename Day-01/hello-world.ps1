2726eecc8dd8f0791976eda72f3ffda037ae6fd7
you <you@examp>
2023-06-05 20:52:41 +0530

    day-1

--- Day-01/hello-world.ps1
+++ Day-01/hello-world.ps1
@@ -1,4 +1,7 @@
 Write-Host "Hello, World..!"
 Write-Host "~ this is last line will execute.." 
 
-$PSVersionTable
+$PSVersionTable # powershell version, as well as platform & edition.
+
+
+$PSVersionTable.PSVersion  # can filter with specifing the details need to filter.
